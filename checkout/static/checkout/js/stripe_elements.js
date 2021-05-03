996a573339d52ab54ce726ff704c311a923f347f
mmmangooo <isabelle.angsas@gmail.com>
2021-05-03 11:37:49 +0000

    Styled payment form

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,17 +1,19 @@
-/* Code comes mostly from https://stripe.com/docs/payments/integration-builder
+/* Code comes mostly from https
+://stripe.com/docs/payments/integration-builder
 and from https://github.com/Code-Institute-Solutions/boutique_ado_v1/blob/f5880efee43b3b9ea1276a09ca972f4588001c59/checkout/static/checkout/js/stripe_elements.js
 */
 
-var stripePublicKey = document.getElementById('id_stripe_public_key').text().slice(1, -1);
-var clientSecret = document.getElementById('id_client_secret').textContent().slice(1, -1);
+var stripePublicKey = document.getElementById('id_stripe_public_key').toString().slice(1, -1);
+var clientSecret = document.getElementById('id_client_secret').toString().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
     base: {
-        color: '#5a5a5a;',
+        color: '#5a5a5a',
         fontFamily: '"Quicksand", sans-serif',
         fontSmoothing: 'antialiased',
         fontSize: '1rem',
+        padding: '1rem',
     },
     invalid: {
         color: '#f55742',
