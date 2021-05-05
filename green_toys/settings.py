4840145dba67146035511800f9e45eb2ff83aa49
mmmangooo <isabelle.angsas@gmail.com>
2021-05-05 13:07:04 +0000

    Removed secret key and set debug

--- green_toys/settings.py
+++ green_toys/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@gyx+5lvc7q@b-g1fv%pb-jx#se@z8*9c761$=p&zxgccd1ngp'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['greentoys.herokuapp.com', 'localhost']
 
