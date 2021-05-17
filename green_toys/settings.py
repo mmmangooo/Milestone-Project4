a74b75d130301697df256976b08584a9edea43c5
mmmangooo <isabelle.angsas@gmail.com>
2021-05-17 12:03:17 +0000

    Corrected another typo in email settings

--- green_toys/settings.py
+++ green_toys/settings.py
@@ -212,12 +212,12 @@ STRIPE_CURRENCY = 'EUR'
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'greentoys@test.com'
+    DEFAULT_FROM_EMAIL = 'greentoys@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_PORT = 587
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASS = os.environ.get('EMAIL_HOST_PASSWORD')
+    EMAIL_HOST_PASS = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
