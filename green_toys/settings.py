42f7390b916365ed2a9238bf63795cb3797a7474
mmmangooo <isabelle.angsas@gmail.com>
2021-05-18 18:06:34 +0000

    Loaded data to postgres

--- green_toys/settings.py
+++ green_toys/settings.py
@@ -128,13 +128,12 @@ WSGI_APPLICATION = 'green_toys.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-       'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
        'default': {
            'ENGINE': 'django.db.backends.sqlite3',
-           'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
        }
      }
 
