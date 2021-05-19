5d8810a918a74bca0686a226cc7b388dbe1b8f0a
mmmangooo <isabelle.angsas@gmail.com>
2021-05-19 20:07:05 +0000

    Migrations

--- green_toys/settings.py
+++ green_toys/settings.py
@@ -137,8 +137,8 @@ else:
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-     }
+    }
+ }
 
 
 # Password validation
