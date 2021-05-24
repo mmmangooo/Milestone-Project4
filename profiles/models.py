5ae2e75b7414846248d7639ff259b75db6f02b3c
mmmangooo <isabelle.angsas@gmail.com>
2021-05-24 09:19:53 +0000

    Fixing responsiveness issues

--- profiles/models.py
+++ profiles/models.py
@@ -29,7 +29,7 @@ class UserProfile(models.Model):
         max_length=80, null=True, blank=True)
 
     def __str__(self):
-        return self.user.username
+        return self.user
 
 
 @receiver(post_save, sender=User)
